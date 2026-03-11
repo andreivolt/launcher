87000fb2d9ba2da48bbc4f2439670bee3f9d5c19
Andrei Volt <andrei@avolt.net>
2026-03-11 21:58:00 +0100

    fix deprecated Hyprland windowrule types for 0.53+
    noborder → border_size 0, noshadow → no_shadow on

--- flake.nix
+++ flake.nix
@@ -101,8 +101,8 @@
                 rules = cls: ''
                   windowrule = workspace special:${cls} silent, match:class ${cls}
                   windowrule = float on, match:class ${cls}
-                  windowrule = noborder on, match:class ${cls}
-                  windowrule = noshadow on, match:class ${cls}
+                  windowrule = border_size 0, match:class ${cls}
+                  windowrule = no_shadow on, match:class ${cls}
                   windowrule = move (monitor_w/2-window_w/2) (monitor_h*0.236), match:class ${cls}
                 '';
               in rules "launcher" + rules "clipboard";
